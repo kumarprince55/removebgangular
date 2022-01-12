8faa159dd31946bfd7da5a0243b218bc80055a59
technicalprince50 <88016406+technicalprince50@users.noreply.github.com>
2022-01-12 20:26:06 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.electionadvisor.in',
+  appName: 'Election Advisor',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
